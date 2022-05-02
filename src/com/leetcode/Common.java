a5a1f59b48336ab94fd14991e44e1760d0db3c97
bigcat <mryux@hotmail.com>
2022-05-02 22:21:28 +1000

    implement lc437 Path Sum III.

--- src/com/leetcode/Common.java
+++ src/com/leetcode/Common.java
@@ -1,6 +1,10 @@
 package com.leetcode;
 
+import com.leetcode.linkedlist.TreeNode;
+
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Common {
     public static int[] ToArray(List<Integer> list) {
@@ -11,5 +15,35 @@ public class Common {
         }
 
         return arr;
+    }
+
+    public static TreeNode Deserialize(Integer[] arr) {
+        if (arr == null || arr.length == 0)
+            return null;
+
+        int idx = 0;
+        TreeNode head = new TreeNode(arr[idx++]);
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(head);
+
+        while (!que.isEmpty()) {
+            int size = que.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = que.poll();
+                if (idx < arr.length && arr[idx] != null) {
+                    node.left = new TreeNode(arr[idx]);
+                    que.add(node.left);
+                }
+                idx++;
+                if (idx < arr.length && arr[idx] != null) {
+                    node.right = new TreeNode(arr[idx]);
+                    que.add(node.right);
+                }
+                idx++;
+            }
+        }
+
+        return head;
     }
 }
---
+++ src/com/leetcode/lc437_Path_Sum_III.java
@@ -0,0 +1,78 @@
+package com.leetcode;
+
+import com.leetcode.linkedlist.TreeNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class lc437_Path_Sum_III {
+    public int pathSum(TreeNode root, int targetSum) {
+        ans = 0;
+        //visit_by_list(root, new ArrayList<>(), targetSum);
+        visit(root, new int[100], 0, targetSum);
+
+        return ans;
+    }
+
+    private int ans = 0;
+    private void visit(TreeNode node, int[] prefixSum, int size, int target) {
+        if (node == null)
+            return;
+
+        int lastVal = 0;
+        if (size > 0) {
+            lastVal = prefixSum[size-1];
+        }
+
+        int currVal = lastVal + node.val;
+        for (int i = 0; i < size; i++) {
+            if (currVal - prefixSum[i] == target) {
+                ans++;
+            }
+        }
+
+        if (currVal == target)
+            ans++;
+
+        prefixSum[size] = currVal;
+        visit(node.left, prefixSum, size+1, target);
+        visit(node.right, prefixSum, size+1, target);
+    }
+
+    private void visit_by_list(TreeNode node, List<Integer> prefixSum, int target) {
+        if (node == null)
+            return;
+
+        int lastVal = 0;
+        int size = prefixSum.size();
+        if (size > 0) {
+            lastVal = prefixSum.get(size-1);
+        }
+
+        // sum from any node in path to this node == target
+        int currVal = lastVal + node.val;
+        for (int i = 0; i < size; i++) {
+            if (currVal - prefixSum.get(i) == target) {
+                ans++;
+            }
+        }
+        // sum from root to this node == target
+        if (currVal == target)
+            ans++;
+
+        prefixSum.add(currVal);
+        visit_by_list(node.left, prefixSum, target);
+        visit_by_list(node.right, prefixSum, target);
+        prefixSum.remove(size);
+    }
+
+    @Test
+    public void test01() {
+        lc437_Path_Sum_III solu = new lc437_Path_Sum_III();
+
+        Assertions.assertEquals(1, solu.pathSum(Common.Deserialize(new Integer[]{1,2}), 2));
+        Assertions.assertEquals(3, solu.pathSum(Common.Deserialize(new Integer[]{5,4,8,11,null,13,4,7,2,null,null,5,1}), 22));
+        Assertions.assertEquals(3, solu.pathSum(Common.Deserialize(new Integer[]{10,5,-3,3,2,null,11,3,-2,null,1}), 8));
+    }
+}
