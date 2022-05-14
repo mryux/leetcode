2f0c0c3f3ea8a2dd60819dccac8f26801a99c8bd
bigcat <mryux@hotmail.com>
2022-05-14 22:06:53 +1000

    implement lc764 Largest Plus Sign.

--- src/com/leetcode/lc764_Largest_Plus_Sign.java
+++ src/com/leetcode/lc764_Largest_Plus_Sign.java
@@ -3,8 +3,6 @@ package com.leetcode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 public class lc764_Largest_Plus_Sign {
     public int orderOfLargestPlusSign(int n, int[][] mines) {
         int[][] dp = new int[n][n];
@@ -17,7 +15,6 @@ public class lc764_Largest_Plus_Sign {
 
         for (int i = 0; i < mines.length; i++)
             dp[mines[i][0]][mines[i][1]] = 0;
-
 
         for (int row = 0; row < n; row++) {
             int count = 0;
